d2dd341b318249b71133f9e8a864bfd8cfebf230
Flumanuck <55293870+Flumanuck@users.noreply.github.com>
2021-10-23 11:33:22 -0400

    added requirements + minor debugging

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const fs = require("fs");
+const db = require("./db/db.json");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
@@ -6,8 +9,18 @@ const PORT = 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
 app.get("/api/notes", (req, res) => {
-  res.send("GET notes");
+  console.log(db);
+  //res.send("GET notes");
+  res.json(db);
 });
 
 app.post("/api/notes", (req, res) => {
